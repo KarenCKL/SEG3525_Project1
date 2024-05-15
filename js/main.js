280a051ca039f2e7c69f561477e9b9d917b39af8
KarenCKL <113635618+KarenCKL@users.noreply.github.com>
2024-05-14 23:37:13 -0400

    new content added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 35,
+        dots: true,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
+});
