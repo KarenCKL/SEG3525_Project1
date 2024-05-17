5d38aa2896ab3bb3f80a5f45ec955eb1b0193be7
KarenCKL <113635618+KarenCKL@users.noreply.github.com>
2024-05-17 14:49:34 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
 $(function () {
-    $('.owl-carousel').owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 35,
-        dots: true,
-        loop: true,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 3
-            },
-            992: {
-                items: 3
-            }
-        }
-    })
+    
 });
